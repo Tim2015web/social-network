e3719ba940ff6dceede02bae98700073b5bb4b34
Ilia Green <tim2015web@gmail.com>
2025-02-24 15:15:46 +0300

    Updates

---
+++ assets/Music-DEqbsAvc.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BaFoLvKo.js";const c={};function e(){return s.jsx("div",{className:c.music,children:s.jsx("p",{children:"Music"})})}export{e as default};
--- assets/Photos-D2nuEGER.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DMyhFxhe.js";const o={};function e(){return s.jsx("div",{className:o.photos,children:s.jsx("p",{children:"Photos"})})}export{e as default};
